a0ecd70684815f9bd0c576ea6045b0918715dd8e
Mumalo <tichamuma@yahoo.com>
2016-03-04 18:23:36 -0800

    Created Fascilitator model

--- tela/models.py
+++ tela/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Fascilitator(models.Model):
+    #center = models.ForeignKey(Center)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return "%s %s" %(self.first_name, self.last_name)
+    
+
+
