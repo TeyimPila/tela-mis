043a7f790734ae56d3fa142d1d60403053f53bb8
Mumalo <tichamuma@yahoo.com>
2016-03-04 18:10:51 -0800

    Center model created

--- tela/models.py
+++ tela/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Center(models.Model):
+    #venue = models.ForeignKey(Venue)
+    title = models.CharField(max_length=50)
+    group_size = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+
+
