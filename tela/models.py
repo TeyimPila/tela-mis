1761d6284d3e29ff40b2bbefcb3b1befc9ad9a93
Mumalo <tichamuma@yahoo.com>
2016-03-04 19:10:06 -0800

    Created Venue model

--- tela/models.py
+++ tela/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    address = models.CharField(max_length=300)
+    longitude = models.CharField(max_length=30)
+    latitude = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.address
+
+    def coordinate(self):
+        return "%s %s" %(self.longitude, self.latitude)
