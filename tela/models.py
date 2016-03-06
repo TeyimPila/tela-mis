4654b31af1d4570d6e886155a632b9d286aafe70
TeyimPila <teyim.pila@aun.edu.ng>
2016-03-06 21:31:12 +0100

    Created the neighborhood model

--- tela/models.py
+++ tela/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Neighborhood(models.Model):
+    neigh_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
