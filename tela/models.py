47c8b63559f8fbceb9c62239a55a90c6bd6be68a
TeyimPila <teyim.pila@aun.edu.ng>
2016-03-08 18:12:23 +0100

    created a test for the neighborhood model

--- tela/models.py
+++ tela/models.py
@@ -8,7 +8,7 @@ class Neighborhood(models.Model):
     This model holds information about the neighborhood from which
     participants of the TELA project come from
     """
-    neigh_id = models.IntegerField(unique=True)
+
     name = models.CharField(max_length=200)
 
     def __str__(self):
--- tela/tests.py
+++ tela/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Neighborhood
 
 # Create your tests here.
+
+
+class TestNeighborhood(TestCase):
+
+    def setUp(self):
+        Neighborhood.objects.create(name="The Neighborhood name")
+
+    def test_neighborhood_creation(self):
+        neigh = Neighborhood.objects.get(name="The Neighborhood name")
+        self.assertTrue(isinstance(neigh, Neighborhood))
+        self.assertEqual(neigh.__str__(), neigh.name)
