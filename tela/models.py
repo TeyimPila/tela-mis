092b631d431812669cda8229fe4edfafd2afe541
TeyimPila <teyim.pila@aun.edu.ng>
2016-03-09 21:41:17 +0100

    created tests for the LocalGovArea model

--- tela/models.py
+++ tela/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class LocalGovArea(models.Model):
+    """
+    This model defines the database table for storing information about the Local Government Areas from which
+    participants come
+    """
+
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
--- tela/tests.py
+++ tela/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import LocalGovArea
 
 # Create your tests here.
+
+
+class TestLGA(TestCase):
+
+    def setUp(self):
+        LocalGovArea.objects.create(name="The local Government Area")
+
+    def test_LGA_creation(self):
+        """
+        this tests that each object of the LocalGovArea model is created properly
+        :return:
+        """
+        lga = LocalGovArea.objects.get(name="The local Government Area")
+        self.assertTrue(isinstance(lga, LocalGovArea))
+
+    def test_LGA_string_representation(self):
+        """
+        this tests that objects of the LocalGovArea have a name that is the same as the name attribute of the local
+        government area
+        :return:
+        """
+        lga = LocalGovArea.objects.get(name="The local Government Area")
+        self.assertEqual(lga.__str__(), lga.name)
