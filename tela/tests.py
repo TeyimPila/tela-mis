49700164c9b1a980efe092ca52b7334247195095
Mumalo <tichamuma@yahoo.com>
2016-03-10 00:09:22 -0800

    Added tests to Center model

--- tela/tests.py
+++ tela/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Center
+class CenterTest(TestCase):
+    def create_venue(self, title='Yola Center', group_size=23):
+        return Center.objects.create(title=title, group_size=group_size)
+
+    def test_venue(self):
+        c = self.create_venue()
+        return self.assertTrue(isinstance(c, Center))
+
+    def test_center_str_method(self):
+        c = Center(title='Yola Center')
+        self.assertEqual(c.__str__(), "Yola Center")
 
 # Create your tests here.
