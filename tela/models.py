5174f591cd6f4b500a7c1431dc2c1126466cf829
Mumalo <tichamuma@yahoo.com>
2016-03-09 22:15:29 -0800

    Added tests to the Venue model

--- tela/models.py
+++ tela/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 class Venue(models.Model):
     address = models.CharField(max_length=300)
-    longitude = models.CharField(max_length=30)
-    latitude = models.CharField(max_length=30)
+    location_latitude = models.CharField('Latitude', max_length=20, null=True, blank=True)
+    location_longitude = models.CharField('latitude', max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return self.address
+        return "%s " % self.address
 
-    def coordinate(self):
-        return "%s %s" %(self.longitude, self.latitude)
+    def get_coordinates(self):
+        return "(%s,%s)" % (self.location_latitude, self.location_longitude)
+
+
+
