0c53856c9af95069775a53a5a1e219834c184aee
Mumalo <tichamuma@yahoo.com>
2016-03-09 23:40:13 -0800

    Added tests to Fascilitator model

--- tela/models.py
+++ tela/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Fascilitator(models.Model):
     #center = models.ForeignKey(Center)
     first_name = models.CharField(max_length=50)
--- tela/tests.py
+++ tela/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Fascilitator
+
+
+class FascilitatorTest(TestCase):
+
+    def create_fascilitator(self, first_name='Fatima', last_name='Hafsat', phone='0813456789', email='example@yahoo.com', age=21):
+        return Fascilitator.objects.create(first_name=first_name, last_name=last_name, phone=phone, email=email, age=age)
+
+    def test_fascilitator(self):
+        f = self.create_fascilitator()
+        self.assertTrue(isinstance(f, Fascilitator))
+
+    def test_fascilitator_str_method(self):
+        f = Fascilitator(first_name='Fatima', last_name='Umar')
+        return self.assertEqual(f.__str__(), "Fatima Umar")
+
 
 # Create your tests here.
