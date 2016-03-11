f201c34ff4e0ce1b01738516b01be3fdc598cb32
TeyimPila <teyim.pila@aun.edu.ng>
2016-03-11 20:46:11 +0100

    created assessment tests

--- tela/models.py
+++ tela/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Beneficiary(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return '%s %s' %(self.first_name, self.last_name)
+
+
+class Assessment (models.Model):
+
+    beneficiary = models.ForeignKey(Beneficiary, on_delete=models.CASCADE)
+    # enumerator = models.ForeignKey(Enumerator, on_delete=models.CASCADE)
+
+    ASSESSMENT_TYPES = (
+        ('Pre-Assessment', 'Pre-assessment'),
+        ('Post-Assessment', 'Post-Assessment'),
+    )
+
+    type = models.CharField(max_length=16, choices=ASSESSMENT_TYPES)
+
+    def __str__(self):
+        return '%s\'s %s' % (self.beneficiary, self.type)
--- tela/tests.py
+++ tela/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Beneficiary
+from .models import Assessment
 
 # Create your tests here.
+
+class TestAssessment(TestCase):
+
+    def setUp(self):
+        self.beneficiary = Beneficiary.objects.create(first_name = "first name", last_name="last name")
+        self.assessment = Assessment.objects.create(beneficiary = self.beneficiary, type="ass type")
+
+    def test_assessment_creation(self):
+        """
+        test that an assessment can be created
+        :return:
+        """
+        self.assertEqual(self.assessment.type, "ass type")
+        self.assertEqual(self.assessment.beneficiary, self.beneficiary)
+
+    def test_assessment_string_representation(self):
+        """
+        test that assessment object is represented as: first_name Last name's Pre assessment
+        :return:
+        """
+        self.assertEqual(self.assessment.__str__(), '%s\'s %s' %(self.beneficiary, self.assessment.type))
+
