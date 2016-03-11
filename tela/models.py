7365959ffa1bc160862b99c20416eb0b05b8609d
TeyimPila <teyim.pila@aun.edu.ng>
2016-03-11 20:58:08 +0100

    deleted dummy beneficiary model

--- tela/models.py
+++ tela/models.py
@@ -1,17 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Beneficiary(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-    def __str__(self):
-        return '%s %s' %(self.first_name, self.last_name)
 
 
 class Assessment (models.Model):
 
-    beneficiary = models.ForeignKey(Beneficiary, on_delete=models.CASCADE)
+    # beneficiary = models.ForeignKey(Beneficiary, on_delete=models.CASCADE)
     # enumerator = models.ForeignKey(Enumerator, on_delete=models.CASCADE)
 
     ASSESSMENT_TYPES = (
