b9c5656327f0e4580875fc598e3e65f1199a1911
Mumalo <tichamuma@yahoo.com>
2016-04-28 00:24:55 -0700

    resolving conflicts

--- TelaMIS/settings.py
+++ TelaMIS/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'TelaMIS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'account' 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
